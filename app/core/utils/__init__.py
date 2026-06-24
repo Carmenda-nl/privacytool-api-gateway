aa21a2c27ab1cc8878bf11fbb745596e973ae44d
Django Heimgartner <django.heimgartner@gmail.com>
2026-06-24 19:27:53 +0200

    clean: remove old core utils

--- app/core/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------------------------ #
-# Copyright (c) 2026 Carmenda. All rights reserved.                                                #
-# This program is distributed under the terms of the GNU General Public License: GPL-3.0-or-later  #
-# ------------------------------------------------------------------------------------------------ #
-
-"""Utilities package.
-
-This package provides utility and helper functions for pseudonymizing data.
-"""
