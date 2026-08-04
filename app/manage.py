a854fdc47ff4ce860d3818fa3c9519c0a2c9936e
Django Heimgartner <django.heimgartner@gmail.com>
2026-08-04 22:23:07 +0200

    bug: version still reports beta after promoting to stable

--- app/manage.py
+++ app/manage.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# ------------------------------------------------------------------------------------------------ #
+# Copyright (c) 2026 Carmenda. All rights reserved.                                                #
+# This program is distributed under the terms of the PolyForm Noncommercial License 1.0.0          #
+# ------------------------------------------------------------------------------------------------ #
+
 """Django's command-line utility for administrative tasks."""
 
 import os
